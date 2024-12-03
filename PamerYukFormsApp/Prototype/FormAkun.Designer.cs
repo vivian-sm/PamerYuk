19a7c0208aeee5b6bffbc7cf3f6227225dc8119e
alpinnbtk <ambrosiusalvin20@gmail.com>
2024-12-03 13:41:59 +0700

    Design form udah kulengkapin

--- PamerYukFormsApp/Prototype/FormAkun.Designer.cs
+++ PamerYukFormsApp/Prototype/FormAkun.Designer.cs
@@ -28,19 +28,257 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonEditProfil = new System.Windows.Forms.Button();
+            this.listBoxKisahHidup = new System.Windows.Forms.ListBox();
+            this.labelKisahHidup = new System.Windows.Forms.Label();
+            this.textBoxTanggalLahir = new System.Windows.Forms.TextBox();
+            this.labelTanggalLahir = new System.Windows.Forms.Label();
+            this.textBoxKota = new System.Windows.Forms.TextBox();
+            this.labelKota = new System.Windows.Forms.Label();
+            this.textBoxNoKTP = new System.Windows.Forms.TextBox();
+            this.labelNoKTP = new System.Windows.Forms.Label();
+            this.labelNama = new System.Windows.Forms.Label();
+            this.textBoxNama = new System.Windows.Forms.TextBox();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.buttonUbah = new System.Windows.Forms.Button();
+            this.panelFotoProfil = new System.Windows.Forms.Panel();
+            this.labelFormAkun = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // buttonEditProfil
+            // 
+            this.buttonEditProfil.Location = new System.Drawing.Point(1195, 825);
+            this.buttonEditProfil.Name = "buttonEditProfil";
+            this.buttonEditProfil.Size = new System.Drawing.Size(158, 48);
+            this.buttonEditProfil.TabIndex = 98;
+            this.buttonEditProfil.Text = "Edit Profil";
+            this.buttonEditProfil.UseVisualStyleBackColor = true;
+            // 
+            // listBoxKisahHidup
+            // 
+            this.listBoxKisahHidup.FormattingEnabled = true;
+            this.listBoxKisahHidup.ItemHeight = 25;
+            this.listBoxKisahHidup.Location = new System.Drawing.Point(643, 616);
+            this.listBoxKisahHidup.Name = "listBoxKisahHidup";
+            this.listBoxKisahHidup.Size = new System.Drawing.Size(601, 179);
+            this.listBoxKisahHidup.TabIndex = 97;
+            // 
+            // labelKisahHidup
+            // 
+            this.labelKisahHidup.AutoSize = true;
+            this.labelKisahHidup.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.labelKisahHidup.Location = new System.Drawing.Point(348, 616);
+            this.labelKisahHidup.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelKisahHidup.Name = "labelKisahHidup";
+            this.labelKisahHidup.Size = new System.Drawing.Size(254, 44);
+            this.labelKisahHidup.TabIndex = 96;
+            this.labelKisahHidup.Text = "Kisah Hidup : ";
+            // 
+            // textBoxTanggalLahir
+            // 
+            this.textBoxTanggalLahir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.textBoxTanggalLahir.Location = new System.Drawing.Point(634, 540);
+            this.textBoxTanggalLahir.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxTanggalLahir.Name = "textBoxTanggalLahir";
+            this.textBoxTanggalLahir.PasswordChar = '*';
+            this.textBoxTanggalLahir.Size = new System.Drawing.Size(601, 50);
+            this.textBoxTanggalLahir.TabIndex = 95;
+            // 
+            // labelTanggalLahir
+            // 
+            this.labelTanggalLahir.AutoSize = true;
+            this.labelTanggalLahir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.labelTanggalLahir.Location = new System.Drawing.Point(348, 540);
+            this.labelTanggalLahir.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelTanggalLahir.Name = "labelTanggalLahir";
+            this.labelTanggalLahir.Size = new System.Drawing.Size(282, 44);
+            this.labelTanggalLahir.TabIndex = 94;
+            this.labelTanggalLahir.Text = "Tanggal Lahir : ";
+            // 
+            // textBoxKota
+            // 
+            this.textBoxKota.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.textBoxKota.Location = new System.Drawing.Point(634, 465);
+            this.textBoxKota.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxKota.Name = "textBoxKota";
+            this.textBoxKota.PasswordChar = '*';
+            this.textBoxKota.Size = new System.Drawing.Size(601, 50);
+            this.textBoxKota.TabIndex = 93;
+            // 
+            // labelKota
+            // 
+            this.labelKota.AutoSize = true;
+            this.labelKota.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.labelKota.Location = new System.Drawing.Point(348, 465);
+            this.labelKota.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelKota.Name = "labelKota";
+            this.labelKota.Size = new System.Drawing.Size(128, 44);
+            this.labelKota.TabIndex = 92;
+            this.labelKota.Text = "Kota : ";
+            // 
+            // textBoxNoKTP
+            // 
+            this.textBoxNoKTP.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.textBoxNoKTP.Location = new System.Drawing.Point(634, 395);
+            this.textBoxNoKTP.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxNoKTP.Name = "textBoxNoKTP";
+            this.textBoxNoKTP.PasswordChar = '*';
+            this.textBoxNoKTP.Size = new System.Drawing.Size(601, 50);
+            this.textBoxNoKTP.TabIndex = 91;
+            // 
+            // labelNoKTP
+            // 
+            this.labelNoKTP.AutoSize = true;
+            this.labelNoKTP.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.labelNoKTP.Location = new System.Drawing.Point(348, 395);
+            this.labelNoKTP.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelNoKTP.Name = "labelNoKTP";
+            this.labelNoKTP.Size = new System.Drawing.Size(181, 44);
+            this.labelNoKTP.TabIndex = 90;
+            this.labelNoKTP.Text = "No KTP : ";
+            // 
+            // labelNama
+            // 
+            this.labelNama.AutoSize = true;
+            this.labelNama.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.labelNama.Location = new System.Drawing.Point(348, 317);
+            this.labelNama.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelNama.Name = "labelNama";
+            this.labelNama.Size = new System.Drawing.Size(152, 44);
+            this.labelNama.TabIndex = 89;
+            this.labelNama.Text = "Nama : ";
+            // 
+            // textBoxNama
+            // 
+            this.textBoxNama.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.textBoxNama.Location = new System.Drawing.Point(634, 317);
+            this.textBoxNama.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxNama.Name = "textBoxNama";
+            this.textBoxNama.Size = new System.Drawing.Size(601, 50);
+            this.textBoxNama.TabIndex = 88;
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.labelEmail.Location = new System.Drawing.Point(348, 237);
+            this.labelEmail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(147, 44);
+            this.labelEmail.TabIndex = 87;
+            this.labelEmail.Text = "Email : ";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.textBoxEmail.Location = new System.Drawing.Point(634, 234);
+            this.textBoxEmail.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(601, 50);
+            this.textBoxEmail.TabIndex = 86;
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.labelUsername.Location = new System.Drawing.Point(348, 156);
+            this.labelUsername.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(226, 44);
+            this.labelUsername.TabIndex = 85;
+            this.labelUsername.Text = "Username : ";
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.textBoxUsername.Location = new System.Drawing.Point(634, 156);
+            this.textBoxUsername.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(601, 50);
+            this.textBoxUsername.TabIndex = 84;
+            // 
+            // buttonUbah
+            // 
+            this.buttonUbah.Location = new System.Drawing.Point(80, 435);
+            this.buttonUbah.Name = "buttonUbah";
+            this.buttonUbah.Size = new System.Drawing.Size(108, 46);
+            this.buttonUbah.TabIndex = 83;
+            this.buttonUbah.Text = "Ubah";
+            this.buttonUbah.UseVisualStyleBackColor = true;
+            // 
+            // panelFotoProfil
+            // 
+            this.panelFotoProfil.Location = new System.Drawing.Point(80, 156);
+            this.panelFotoProfil.Margin = new System.Windows.Forms.Padding(2);
+            this.panelFotoProfil.Name = "panelFotoProfil";
+            this.panelFotoProfil.Size = new System.Drawing.Size(225, 242);
+            this.panelFotoProfil.TabIndex = 82;
+            // 
+            // labelFormAkun
+            // 
+            this.labelFormAkun.AutoSize = true;
+            this.labelFormAkun.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Bold);
+            this.labelFormAkun.Location = new System.Drawing.Point(495, 22);
+            this.labelFormAkun.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelFormAkun.Name = "labelFormAkun";
+            this.labelFormAkun.Size = new System.Drawing.Size(466, 97);
+            this.labelFormAkun.TabIndex = 81;
+            this.labelFormAkun.Text = "Akun Saya";
+            this.labelFormAkun.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // FormAkun
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1888, 992);
+            this.ClientSize = new System.Drawing.Size(1416, 893);
+            this.Controls.Add(this.buttonEditProfil);
+            this.Controls.Add(this.listBoxKisahHidup);
+            this.Controls.Add(this.labelKisahHidup);
+            this.Controls.Add(this.textBoxTanggalLahir);
+            this.Controls.Add(this.labelTanggalLahir);
+            this.Controls.Add(this.textBoxKota);
+            this.Controls.Add(this.labelKota);
+            this.Controls.Add(this.textBoxNoKTP);
+            this.Controls.Add(this.labelNoKTP);
+            this.Controls.Add(this.labelNama);
+            this.Controls.Add(this.textBoxNama);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.buttonUbah);
+            this.Controls.Add(this.panelFotoProfil);
+            this.Controls.Add(this.labelFormAkun);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormAkun";
             this.Text = "FormAkun";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonEditProfil;
+        private System.Windows.Forms.ListBox listBoxKisahHidup;
+        private System.Windows.Forms.Label labelKisahHidup;
+        private System.Windows.Forms.TextBox textBoxTanggalLahir;
+        private System.Windows.Forms.Label labelTanggalLahir;
+        private System.Windows.Forms.TextBox textBoxKota;
+        private System.Windows.Forms.Label labelKota;
+        private System.Windows.Forms.TextBox textBoxNoKTP;
+        private System.Windows.Forms.Label labelNoKTP;
+        private System.Windows.Forms.Label labelNama;
+        private System.Windows.Forms.TextBox textBoxNama;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Button buttonUbah;
+        private System.Windows.Forms.Panel panelFotoProfil;
+        private System.Windows.Forms.Label labelFormAkun;
     }
 }
--- PamerYukFormsApp/Prototype/FormTambahKonten.Designer.cs
+++ PamerYukFormsApp/Prototype/FormTambahKonten.Designer.cs
@@ -28,19 +28,151 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonGantiVideo = new System.Windows.Forms.Button();
+            this.buttonGantiGambar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonTambah = new System.Windows.Forms.Button();
+            this.listBoxDeskripsi = new System.Windows.Forms.ListBox();
+            this.labelDeskripsi = new System.Windows.Forms.Label();
+            this.panelVideo = new System.Windows.Forms.Panel();
+            this.labelVideo = new System.Windows.Forms.Label();
+            this.labelGambar = new System.Windows.Forms.Label();
+            this.labelFormTambahKonten = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // buttonGantiVideo
+            // 
+            this.buttonGantiVideo.Location = new System.Drawing.Point(1054, 295);
+            this.buttonGantiVideo.Name = "buttonGantiVideo";
+            this.buttonGantiVideo.Size = new System.Drawing.Size(155, 66);
+            this.buttonGantiVideo.TabIndex = 75;
+            this.buttonGantiVideo.Text = "Ganti Video";
+            this.buttonGantiVideo.UseVisualStyleBackColor = true;
+            // 
+            // buttonGantiGambar
+            // 
+            this.buttonGantiGambar.Location = new System.Drawing.Point(320, 295);
+            this.buttonGantiGambar.Name = "buttonGantiGambar";
+            this.buttonGantiGambar.Size = new System.Drawing.Size(155, 66);
+            this.buttonGantiGambar.TabIndex = 74;
+            this.buttonGantiGambar.Text = "Ganti Gambar";
+            this.buttonGantiGambar.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(45, 224);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(225, 242);
+            this.panel1.TabIndex = 73;
+            // 
+            // buttonTambah
+            // 
+            this.buttonTambah.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.buttonTambah.Location = new System.Drawing.Point(1209, 748);
+            this.buttonTambah.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonTambah.Name = "buttonTambah";
+            this.buttonTambah.Size = new System.Drawing.Size(170, 51);
+            this.buttonTambah.TabIndex = 72;
+            this.buttonTambah.Text = "Tambah";
+            this.buttonTambah.UseVisualStyleBackColor = true;
+            // 
+            // listBoxDeskripsi
+            // 
+            this.listBoxDeskripsi.FormattingEnabled = true;
+            this.listBoxDeskripsi.ItemHeight = 25;
+            this.listBoxDeskripsi.Location = new System.Drawing.Point(45, 570);
+            this.listBoxDeskripsi.Name = "listBoxDeskripsi";
+            this.listBoxDeskripsi.Size = new System.Drawing.Size(1334, 154);
+            this.listBoxDeskripsi.TabIndex = 71;
+            // 
+            // labelDeskripsi
+            // 
+            this.labelDeskripsi.AutoSize = true;
+            this.labelDeskripsi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.labelDeskripsi.Location = new System.Drawing.Point(37, 502);
+            this.labelDeskripsi.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelDeskripsi.Name = "labelDeskripsi";
+            this.labelDeskripsi.Size = new System.Drawing.Size(207, 44);
+            this.labelDeskripsi.TabIndex = 70;
+            this.labelDeskripsi.Text = "Deskripsi : ";
+            // 
+            // panelVideo
+            // 
+            this.panelVideo.Location = new System.Drawing.Point(770, 224);
+            this.panelVideo.Margin = new System.Windows.Forms.Padding(2);
+            this.panelVideo.Name = "panelVideo";
+            this.panelVideo.Size = new System.Drawing.Size(225, 242);
+            this.panelVideo.TabIndex = 69;
+            // 
+            // labelVideo
+            // 
+            this.labelVideo.AutoSize = true;
+            this.labelVideo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.labelVideo.Location = new System.Drawing.Point(762, 144);
+            this.labelVideo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelVideo.Name = "labelVideo";
+            this.labelVideo.Size = new System.Drawing.Size(147, 44);
+            this.labelVideo.TabIndex = 68;
+            this.labelVideo.Text = "Video : ";
+            // 
+            // labelGambar
+            // 
+            this.labelGambar.AutoSize = true;
+            this.labelGambar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.labelGambar.Location = new System.Drawing.Point(37, 144);
+            this.labelGambar.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelGambar.Name = "labelGambar";
+            this.labelGambar.Size = new System.Drawing.Size(189, 44);
+            this.labelGambar.TabIndex = 67;
+            this.labelGambar.Text = "Gambar : ";
+            // 
+            // labelFormTambahKonten
+            // 
+            this.labelFormTambahKonten.AutoSize = true;
+            this.labelFormTambahKonten.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Bold);
+            this.labelFormTambahKonten.Location = new System.Drawing.Point(377, 2);
+            this.labelFormTambahKonten.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelFormTambahKonten.Name = "labelFormTambahKonten";
+            this.labelFormTambahKonten.Size = new System.Drawing.Size(668, 97);
+            this.labelFormTambahKonten.TabIndex = 66;
+            this.labelFormTambahKonten.Text = "Tambah Konten";
+            this.labelFormTambahKonten.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // FormTambahKonten
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1888, 992);
+            this.ClientSize = new System.Drawing.Size(1416, 822);
+            this.Controls.Add(this.buttonGantiVideo);
+            this.Controls.Add(this.buttonGantiGambar);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonTambah);
+            this.Controls.Add(this.listBoxDeskripsi);
+            this.Controls.Add(this.labelDeskripsi);
+            this.Controls.Add(this.panelVideo);
+            this.Controls.Add(this.labelVideo);
+            this.Controls.Add(this.labelGambar);
+            this.Controls.Add(this.labelFormTambahKonten);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormTambahKonten";
             this.Text = "FormTambahKonten";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonGantiVideo;
+        private System.Windows.Forms.Button buttonGantiGambar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonTambah;
+        private System.Windows.Forms.ListBox listBoxDeskripsi;
+        private System.Windows.Forms.Label labelDeskripsi;
+        private System.Windows.Forms.Panel panelVideo;
+        private System.Windows.Forms.Label labelVideo;
+        private System.Windows.Forms.Label labelGambar;
+        private System.Windows.Forms.Label labelFormTambahKonten;
     }
 }

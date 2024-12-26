142808ba88cf3d1b2964027b0de84f71b343f031
dFaery <137483979+dFaery@users.noreply.github.com>
2024-12-26 18:23:49 +0700

    Menambahkan Open File Dialog di FormDaftar

--- PamerYukFormsApp/Prototype/FormDaftar.cs
+++ PamerYukFormsApp/Prototype/FormDaftar.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,11 +26,15 @@ namespace PamerYukFormsApp.Prototype
             string confirm_password = textBoxKonfirmasi.Text;
             Kota selected_kota = (Kota)comboBoxKota.SelectedItem;
             DateTime tanggalLahir = dateTimePicker1.Value.Date;
-            string profile_picture = "null.jpg"; //untuk sementara ku ga isi picture nya belum belajar buat path file di C#
-            string noKTP = textBoxNoKTP.Text;
-            if(CheckPassword(password,confirm_password))
+            //Belum pasti
+            string profile_picture = fileDialog.FileName; //untuk sementara ku ga isi picture nya belum belajar buat path file di C#
+            string noKTP = textBoxNoKTP.Text;                                    
+
+            if (CheckPassword(password,confirm_password))
             {
                 FormUtama.service.Current_user = FormUtama.service.Daftar(username, password, tanggalLahir, noKTP, profile_picture, selected_kota);
+
+                //FormUtama.service.Current_user = FormUtama.service.Daftar(username, password, tanggalLahir, noKTP, fileDialog, selected_kota);
                 this.Close();
             }
             else
@@ -50,5 +55,34 @@ namespace PamerYukFormsApp.Prototype
             return pass == confirm_pass;
         }
         #endregion
+
+        //Ini untuk class FileDialognya --> public biar dia gampang dipanggil
+        OpenFileDialog fileDialog;
+        
+        private void buttonUploadImage_Click(object sender, EventArgs e)
+        {            
+            try
+            {
+                fileDialog = new OpenFileDialog();
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    if (Path.GetExtension(fileDialog.FileName) == ".jpg")
+                    {
+                        Image selectedImage = new Bitmap(fileDialog.FileName);
+                        panelFoto.BackgroundImage = selectedImage;
+                        panelFoto.BackgroundImageLayout = ImageLayout.Zoom;
+                        panelFoto.Visible = true;
+                    }                    
+                    else
+                    {
+                        MessageBox.Show("Only support jpg.");
+                    }
+                }                
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
